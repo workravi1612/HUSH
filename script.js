4936c7224b7278cc8b304ac5deb3622cbf4cc082
ravi <work.ravivyas@gmail.com>
2024-12-16 15:13:01 +0530

    Web

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  document.getElementById("scroll-link").addEventListener("click", function (e) {
+    e.preventDefault(); 
+    // Prevent default anchor behavior
+    document.getElementById("form-section").scrollIntoView({ behavior: "smooth" });
+  });
+
+document.getElementById("register-btn").addEventListener("click", function (e) {
+    e.preventDefault(); 
+    // Prevent default anchor behavior
+    document.getElementById("form-section").scrollIntoView({ behavior: "smooth" });
+  });
